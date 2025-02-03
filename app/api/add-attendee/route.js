d8dbfe49b53de6f308411c936b0ce4fb0a18eb94
AbdulSamad18090 <abdulsamad18090@gmail.com>
2025-02-03 13:10:34 +0500

    updated add attendee api

--- app/api/add-attendee/route.js
+++ app/api/add-attendee/route.js
@@ -1,10 +1,35 @@
+import Event from "@/lib/models/Event";
 import { NextResponse } from "next/server";
 
-export async function PUT(req) {
+export async function PATCH(req) {
   try {
     const body = await req.json();
     const transaction = await JSON.parse(body);
+    const { eventId, customerId, customerEmail, tickets, totalAmount } =
+      transaction;
     console.log("Transaction =>", transaction);
+    const existingEvent = await Event.findById(eventId);
+    if (!existingEvent) {
+      return NextResponse.json(
+        { message: "Event not found." },
+        { status: 404 }
+      );
+    }
+    // Ensure attendees array exists
+    existingEvent.attendees = existingEvent.attendees || [];
+    existingEvent.attendees.push({
+      eventId,
+      customerId,
+      customerEmail,
+      tickets,
+      totalAmount,
+    });
+
+    await existingEvent.save();
+    return NextResponse.json(
+      { message: "Attendee added successfully." },
+      { status: 200 }
+    );
   } catch (error) {
     return NextResponse.json(
       {
